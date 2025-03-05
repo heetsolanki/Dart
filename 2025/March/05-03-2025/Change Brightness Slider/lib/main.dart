0a7e02134b7b089f4fdbb7bec54e50a1989cc241
heetsolanki <heetsolanki2006@gmail.com>
2025-03-05 11:10:17 +0530

    Create Change Brightness Slider

---
+++ 2025/March/05-03-2025/Change Brightness Slider/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title = 'Calculate Grade';
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+  // This class is the configuration for the state.
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double brightness = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: const Text('Change Brightness'),
+        elevation: 20.0, //<Widget>[]
+
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.black,
+      ),
+      body: Container(
+        color: Colors.white,
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Change Brightness',
+              style: TextStyle(fontSize: 20),
+            ),
+            Slider(
+                value: brightness,
+                min: 10,
+                max: 100,
+                label: brightness.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    brightness = value;
+                  });
+                }),
+            Text('Brightness: ${brightness.round()}',
+                style: const TextStyle(fontSize: 25))
+          ],
+        ),
+      ),
+    );
+  }
+}
