897af6d7bcc68936896ba3f1a841413212eb5535
heetsolanki <heetsolanki2006@gmail.com>
2025-03-12 21:12:21 +0530

    Create Years Picker

---
+++ 2025/March/12-03-2025/Years Picker/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:date_picker_plus/date_picker_plus.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String _title = 'Budget Slider';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        // useMaterial3: false,
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime _selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Budget Slider',
+            style: TextStyle(fontWeight: FontWeight.w500)),
+        leading: const Icon(Icons.numbers),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          YearsPicker(
+            selectedDate: _selectedDate,
+            minDate: DateTime(2010),
+            maxDate: DateTime.now(),
+            onDateSelected: (date) {
+              setState(() {
+                _selectedDate = date;
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Text(
+              'You chose: $_selectedDate',
+              style: const TextStyle(fontSize: 18),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
